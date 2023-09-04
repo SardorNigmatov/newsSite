ddcc29c9562490b0f1b9a75292a1f0d08e56a007
SardorNigmatov <sardornigmatov2003@gmail.com>
2023-09-04 09:55:10 +0500

    yangilandi

--- config/settings.py
+++ config/settings.py
@@ -11,21 +11,23 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u-x%kxc^s!xc=t^l6morursk5xqs_3b@u#mvx+x##fr&8w4x6f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['107.23.147.231']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -79,14 +81,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+SECRET_KEY = os.environ.get('SECRET_KEY') #here
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'news',
-        'USER': 'sardor',
-        'PASSWORD': '2123854',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'), #here
+        'USER': os.environ.get('DB_USER'), #here
+        'PASSWORD': os.environ.get('DB_PASS'), #here
         'HOST': 'localhost',
-        'PORT': '',
+        'PORT': '5432'
     }
 }
 
@@ -127,8 +130,8 @@ USE_TZ = True
 
 import os
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "home/ubuntu/news_project/newsSite/static"]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = [BASE_DIR / "static"]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 AUTH_USER_MODEL = 'users.CustomUser'
 
 # Default primary key field type
